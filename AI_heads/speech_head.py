de6a88958f27882fb4dd4491305ac24041c5b51e
A-glitch-bat <79161494+A-glitch-bat@users.noreply.github.com>
2025-01-03 20:19:57 +0100

    Running speech head on separate thread

--- AI_heads/speech_head.py
+++ AI_heads/speech_head.py
@@ -2,7 +2,7 @@
 
 # Imports
 import speech_recognition as sr
-#--------------------------------
+from PyQt5.QtCore import QObject, pyqtSignal
 """
 pip install SpeechRecognition
 pip install PyAudio
@@ -12,23 +12,35 @@ print("Available microphones:")
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print(f"{index}: {name}")
 """
-voice_recognizer = sr.Recognizer()
+#--------------------------------
 
-def listener():
-    with sr.Microphone(device_index=1) as source:
-        print("Adjusting for ambient noise... Please wait.")
-        voice_recognizer.adjust_for_ambient_noise(source)
-        print("Speak something!")
-        try:
-            audio = voice_recognizer.listen(source)
-            text = voice_recognizer.recognize_google(audio)
-            print(f"You said: {text}")
-        except sr.WaitTimeoutError:
-            print("No speech detected within the timeout period.")
-        except sr.UnknownValueError:
-            print("Speech Recognition could not understand the audio.")
-        except sr.RequestError as e:
-            print(f"Could not request results from Google API: {e}")
+# Speech head class
+class SpeechHead(QObject):
+    # ready to send response signal to main thread
+    text_detected = pyqtSignal(str)
 
-if __name__ == "__main__":
-    audio = listener()
+    def __init__(self):
+        super(SpeechHead, self).__init__()
+        self.voice_recognizer = sr.Recognizer()
+        self.device_index = 1
+        self.running = True
+
+    def listen(self):
+        while self.running:
+            try:
+                with sr.Microphone(device_index=self.device_index) as source:
+                    self.voice_recognizer.adjust_for_ambient_noise(source)
+                    audio = self.voice_recognizer.listen(source, timeout=5, phrase_time_limit=10)
+                    text = self.voice_recognizer.recognize_google(audio)
+                    #print(f"Detected speech: {text}")
+                    self.text_detected.emit(text) # signal to main thread
+            except sr.UnknownValueError:
+                self.text_detected.emit("I didn't quite catch that.")
+            except sr.RequestError as e:
+                self.text_detected.emit(f"API error: {str(e)}")
+            except Exception as e:
+                self.text_detected.emit(f"Exception error: {str(e)}")
+
+    def stop(self):
+        self.running = False
+#--------------------------------
