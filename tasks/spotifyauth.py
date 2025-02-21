4592e9d3735d74304b30b152e6246f129d954e54
A-glitch-bat <ag3507@student.uni-lj.si>
2025-02-21 15:00:49 +0100

    Spotify auth & API

--- tasks/spotifyauth.py
+++ tasks/spotifyauth.py
@@ -1,18 +1,52 @@
+#--------------------------------
+
+# Imports
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import sys
+import os
 
-# Set up Spotify authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="YOUR_CLIENT_ID",
-    client_secret="YOUR_CLIENT_SECRET",
-    redirect_uri="http://localhost:8080",
-    scope="user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
-))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import config
+#--------------------------------
 
-# Get current playback state
-current_track = sp.current_playback()
+# Spotify API
+class SpotifyAPI():
+    def __init__(self):
+        super(SpotifyAPI, self).__init__()
 
-if current_track:
-    print(f"Now playing: {current_track['item']['name']} by {current_track['item']['artists'][0]['name']}")
-else:
-    print("No song is currently playing.")
+        # Spotify authentication
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=config.spotify_client_id,
+            client_secret=config.spotify_client_secret,
+            redirect_uri="http://localhost:5000",
+            scope="user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
+        ))
+    #--------------------------------
+    # Functions
+
+    def get_current_song(self):
+        """
+        get stats
+        """
+        current_track = self.sp.current_playback()
+        if current_track:
+            print(f"Now playing: {current_track['item']['name']} by {current_track['item']['artists'][0]['name']}")
+        else:
+            print("No song is currently playing.")
+    #--------------------------------
+
+    def play_track(self, keywords):
+        """
+        play track from keywords
+        """
+        results = self.sp.search(q=keywords, type="track", limit=1)
+
+        # get URI and play
+        if results["tracks"]["items"]:
+            track_uri = results["tracks"]["items"][0]["uri"]
+            self.sp.start_playback(uris=[track_uri])
+            print(f"Playing: {results['tracks']['items'][0]['name']}")
+        else:
+            print("No track found.")
+#--------------------------------
