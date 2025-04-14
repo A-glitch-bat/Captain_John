c4c6c5c8d0bcb8c628a1f7e5bde2b4693172fef2
A-glitch-bat <ag3507@student.uni-lj.si>
2025-04-14 22:14:01 +0200

    Added CSS and some tweaks

--- rust_console/src/main.rs
+++ rust_console/src/main.rs
@@ -1,15 +1,12 @@
-use axum::{Router, routing::get};
-use std::{net::SocketAddr, fs};
-use axum::response::Html;
-
-async fn serve_html() -> Html<String> {
-    let html_content = fs::read_to_string("index.html").unwrap_or_else(|_| "<h1>Failed to load HTML</h1>".to_string());
-    Html(html_content)
-}
+use axum::{Router, routing::get, response::Html};
+use tower_http::services::ServeDir;
+use std::{fs, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(serve_html));
+    let app = Router::new()
+        .route("/", get(serve_html))
+        .nest_service("/static", ServeDir::new("static"));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on http://{}", addr);
@@ -17,4 +14,9 @@ async fn main() {
     axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
         .await
         .unwrap();
+}
+
+async fn serve_html() -> Html<String> {
+    let html = fs::read_to_string("index.html").unwrap_or_else(|_| "<h1>Failed to load HTML</h1>".to_string());
+    Html(html)
 }
